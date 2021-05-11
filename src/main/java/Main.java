92b5e0d42da634a82e7a2b76fef467a66f1150ff
LennartBendfeldt <46977057+LennartBendfeldt@users.noreply.github.com>
2021-05-11 14:01:00 -0400

    small changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,24 +5,23 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //Declaring our variables
         String firstName;
         String lastName;
         String phoneNumber;
         String emailAddress;
 
-        // Declared Array list & passed the AddressBook class as an object
-        //this will act as a database for this project
+        // Declaring our address book ArrayList
         ArrayList<AddressBook> addressBook = new ArrayList<AddressBook>();
 
 
-        // Scanner object is initiated with system.in to read the user input
+        // Creating a scanner object to get user input
         Scanner input = new Scanner(System.in);
-        int option = 0;
+        int userInput = 0;
 
 
-        /// This block of code is displaying all the menu to choose
-        //if the option is 6, the programme will quit
-        while (option != 6) {
+        /// Printing the menu
+        while (userInput != 6) {
             System.out.println("1) Add an Entry\n");
             System.out.println("2) Remove an Entry\n");
             System.out.println("3) Search for a Specific Entry\n");
@@ -31,10 +30,10 @@ public class Main {
             System.out.println("6) Quit\n");
             System.out.println("Please choose what you'd like to do with the database:");
 
-            option = input.nextInt();
+            userInput = input.nextInt();
 
-            ////// This block of code(line 35 - line 54) is taking the input from the user & adding it to the Array list
-            if (option == 1) {
+            // Taking user input and adding it to the array list.
+            if (userInput == 1) {
                 Scanner inputAdd = new Scanner(System.in);
                 System.out.println("First Name: ");
                 firstName = inputAdd.nextLine();
@@ -48,10 +47,10 @@ public class Main {
                 System.out.println("Email Address:");
                 emailAddress = inputAdd.nextLine();
 
-                int valid=emailAddress.indexOf("@");
-                int valid2 = emailAddress.indexOf(".");
+                int validationAtSymbol = emailAddress.indexOf("@");
+                int validationDotSeparator = emailAddress.indexOf(".");
 
-                if (valid == -1 || valid2 == -1){
+                if (validationAtSymbol == -1 || validationDotSeparator == -1){
 
                     System.out.println("You have entered an invalid email address!");
                 }
@@ -64,8 +63,8 @@ public class Main {
                 }
             }
 
-            /// This whole block of codes are for removing the entry against specific search( line 59 -line 80)
-            if (option == 2) {
+            // Code to remove a user specified address entry
+            if (userInput == 2) {
 
                 String email;
                 Scanner inputRemove = new Scanner(System.in);
@@ -101,8 +100,8 @@ public class Main {
                 }
             }
 
-            //printing the options of the lists of entries
-            if (option == 3) {
+            //printing the userInputs of the lists of entries
+            if (userInput == 3) {
 
                 System.out.println("1) First Name:");
                 System.out.println("2) Last Name:");
@@ -119,7 +118,6 @@ public class Main {
                 String search = input.next();
 
 
-                //// With the specific type & search type , all the specific elements are displayed
                 for (AddressBook entry : addressBook) {
 
                     if (type == 1) {
@@ -145,7 +143,7 @@ public class Main {
             }
 
             /// Printing all the elements from the Array list
-            if (option == 4) {
+            if (userInput == 4) {
                 System.out.println("Address list:");
                 for (AddressBook entry : addressBook) {
                     System.out.println(entry.toString());
@@ -155,8 +153,8 @@ public class Main {
 
 
 
-            ///Delete All elements from Array list
-            if (option == 5) {
+            ///Delete all address book entries
+            if (userInput == 5) {
                 addressBook.clear();
                 System.out.println("Address deleted successfully\n");
 
